f92a88e0553f155bc7a9da5292b09b987e91e3eb
alejandrodiazpugh <alejandrodiazpugh@gmail.com>
2023-03-18 17:11:27 -0600

    work on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,55 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import { BiMenu } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
 
 type Props = {};
 
 export default function Nav({}: Props) {
+  const BREAKPOINT: number = 767;
+  // CHECK FOR WINDOW SIZE
+  const getWindowDimensions = () => {
+    if (typeof window === "undefined") {
+      //NEXT JS SSG CHECK
+      return;
+    }
+    const { innerWidth: width } = window;
+    return width;
+  };
+  // STATE
+  const [navOpen, setNavOpen] = useState(false);
+  const [windowDimension, setWindowDimension] = useState(getWindowDimensions());
+
+  // HANDLE CLICK FOR NAV
+  const handleClick = () => {
+    setNavOpen(!navOpen);
+  };
+
+  useEffect(() => {
+    // UPDATE VALUE FOR WINDOW SIZE
+    window.addEventListener("resize", () => {
+      setWindowDimension(getWindowDimensions());
+    });
+
+    // CHECK FOR BREAKPOINT TO CLOSE THE NAVBAR
+    const width = getWindowDimensions();
+    if (width && width > BREAKPOINT) {
+      setNavOpen(false);
+    }
+  }, [windowDimension]);
   return (
     <header>
       <Image src="/cp-06.png" alt="" priority width="50" height="50" />
-      <nav>
-        <ul role="list" className="nav-items">
+
+      <nav className="mobile-nav">
+        {navOpen ? (
+          <IoClose fill={"white"} onClick={() => handleClick()} />
+        ) : (
+          <BiMenu fill={"white"} onClick={() => handleClick()} />
+        )}
+      </nav>
+      <nav className="desktop-nav">
+        <ul role="list" className="nav-items__desktop">
           <li>
             <a href="#location">Lugar y Fecha</a>
           </li>
